464e895061a99b01fdb070dd2bdd6ac5554dddb4
RaffaelCarv <54192511+RaffaelCarv@users.noreply.github.com>
2021-05-20 17:04:46 -0300

    InstalacaoSophosPowerShell.ps1

--- InstalacaoSophosPowerShell.ps1
+++ InstalacaoSophosPowerShell.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 .SYNOPSIS
     Install built-in apps (Sophos) from Windows 10.
 .DESCRIPTION
@@ -28,7 +28,7 @@ Write-Host "Begining the installation"
 # Check if work directory exists if not create it
 
 If (Test-Path -Path $workdir -PathType Container){
-Write-Host "$workdir already exists" -ForegroundColor Red
+Write-Host "$workdir already exists" -ForegroundColor Green
 } ELSE {
 New-Item -Path $workdir -ItemType directory
 }
@@ -40,6 +40,8 @@ $destination = "$workdir\SophosSetup.exe"
 
 # Check if Invoke-Webrequest exists otherwise execute WebClient
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 if (Get-Command 'Invoke-Webrequest'){
 Invoke-WebRequest $source -OutFile $destination
 } else {
@@ -50,7 +52,7 @@ $webclient.DownloadFile($source, $destination)
 # Start the installation
 Start-Process -FilePath "$workdir\SophosSetup.exe" -ArgumentList "--quiet"
 
-Start-Sleep -s 35
+Start-Sleep -s 360
 
 Start-Process -FilePath "C:\Program Files\Sophos\Sophos UI\Sophos UI.exe" -ArgumentList "/AUTO"
 }
