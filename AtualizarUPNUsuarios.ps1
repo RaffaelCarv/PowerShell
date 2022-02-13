536a31a4a2f36e8e493498c1782f49758232d8f8
RaffaelCarv <54192511+RaffaelCarv@users.noreply.github.com>
2022-02-13 11:00:18 -0300

    Update AtualizarUPNUsuarios.ps1
    For the command to work, you need to comment out # Whatif

--- AtualizarUPNUsuarios.ps1
+++ AtualizarUPNUsuarios.ps1
@@ -3,5 +3,5 @@ Get-ADUser -Filter * | foreach {Set-ADUser $_ -Replace @{c="BR";co="Brasil";coun
 Get-ADUser -Filter {UserPrincipalName -like "*@OldSuffix"} -SearchBase "OU=,DC=,DC=" |
 ForEach-Object {
     $UPN = $_.UserPrincipalName.Replace("OldSuffix","NewSuffix")
-    Set-ADUser $_ -UserPrincipalName $UPN #-Verbose -WhatIf
+    Set-ADUser $_ -UserPrincipalName $UPN -Verbose -WhatIf
 }
