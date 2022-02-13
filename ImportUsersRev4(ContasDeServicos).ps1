f7f92e0136bbc4a7194dacd4aaed0992af373d81
RaffaelCarv <54192511+RaffaelCarv@users.noreply.github.com>
2022-02-13 11:10:54 -0300

    Update ImportUsersRev4(ContasDeServicos).ps1

--- ImportUsersRev4(ContasDeServicos).ps1
+++ ImportUsersRev4(ContasDeServicos).ps1
@@ -1,14 +1,13 @@
-﻿cls
 Import-Module ActiveDirectory
 $Arquivo = Read-Host "Nome do Arquivo"
-Import-Csv C:\Users\rafael.carvalho\Desktop\Brasoftware\Users\ContasDeServico\$Arquivo.csv |
+Import-Csv $Arquivo.csv |
 foreach {
          $name = "$($_.SamAccountName) Service"
 		 $secpass = ConvertTo-SecureString -AsPlainText $($_.Password) -Force
          New-ADUser -GivenName $($_.FirstName) -Surname $($_.LastName) `
          -Name $name -SamAccountName "$($_.SamAccountName)" `
          -UserPrincipalName "$($_.SamAccountName)@ADPMS.local" `
-         -AccountPassword $secpass -Path "OU=Contas de Servico,OU=Usuarios,OU=$Arquivo,OU=PMS,DC=ADPMS,DC=local" `
-         -Enabled:$true -Verbose #-WhatIf
+         -AccountPassword $secpass -Path "DC=,DC=" `
+         -Enabled:$true -Verbose -WhatIf
 
 }
