0b406cb9a2e1f06db11cd580afa5f8284bbbb1af
RaffaelCarv <54192511+RaffaelCarv@users.noreply.github.com>
2022-02-13 11:11:56 -0300

    Update MovedUsersBetweenOUs.ps1

--- MovedUsersBetweenOUs.ps1
+++ MovedUsersBetweenOUs.ps1
@@ -5,10 +5,10 @@ Import-Module ActiveDirectory
 $reference = Read-Host "Nome do Arquivo"
  
 # Import the data from CSV file and assign it to variable
-$Import_csv = Import-Csv -Path "C:\Users\rafael.carvalho\Desktop\Brasoftware\Groups\Info\$reference.csv"
+$Import_csv = Import-Csv -Path "$reference.csv"
 
 # Specify target OU where the users will be moved to
-$TargetOU = "OU=Usuarios,OU=$reference,OU=PMS,DC=ADPMS,DC=local"
+$TargetOU = "DC=,DC="
  
 $Import_csv | ForEach-Object {
 
@@ -18,6 +18,6 @@ $Import_csv | ForEach-Object {
     Write-Host "Moving Accounts....."
 
     # Move user to target OU. Remove the -WhatIf parameter after you tested.
-    Move-ADObject -Identity $UserDN -TargetPath $TargetOU #-WhatIf
+    Move-ADObject -Identity $UserDN -TargetPath $TargetOU -WhatIf
 } 
 Write-Host "Completed move"
